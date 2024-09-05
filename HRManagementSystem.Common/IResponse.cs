3b20f34091e6f68575c0b029958a169e2b13aaa9
Enes Doğan <enesdoganlive@hotmail.com>
2024-09-05 11:05:47 +0300

    Initial commit

---
+++ HRManagementSystem.Common/IResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRManagementSystem.Common
+{
+    public interface IResponse
+    {
+        string Message { get; set; }
+        ResponseType ResponseType { get; set; }
+    }
+}
---
+++ HRManagementSystem.Common/Response.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRManagementSystem.Common
+{
+    public class Response : IResponse
+    {
+        public Response(ResponseType responseType)
+        {
+            ResponseType = responseType;
+        }
+
+        public Response(ResponseType responseType, string message)
+        {
+            ResponseType = responseType;
+            Message = message;
+        }
+
+        public string Message { get; set; }
+        public ResponseType ResponseType { get; set; }
+    }
+
+    public enum ResponseType
+    {
+        Success,
+        ValidationError,
+        NotFound
+    }
+}
