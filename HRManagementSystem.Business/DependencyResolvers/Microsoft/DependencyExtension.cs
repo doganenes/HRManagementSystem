1bf40344336c84b9653e9f349959c0b67d5406eb
Enes Doğan <enesdoganlive@hotmail.com>
2024-09-12 11:50:00 +0300

    created business layer

---
+++ HRManagementSystem.Business/DependencyResolvers/Microsoft/DependencyExtension.cs
@@ -0,0 +1,22 @@
+﻿using HRManagementSystem.DataAccess.Concrete;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRManagementSystem.Business.DependencyResolvers.Microsoft
+{
+    public static class DependencyExtension
+    {
+        public static void AddDependencies(this IServiceCollection services)
+        {
+            services.AddDbContext<HRContext>(opt =>
+            {
+                opt.UseSqlServer("");
+            });
+        }
+    }
+}
