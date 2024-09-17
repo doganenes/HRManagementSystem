ed5832ab676ae35bf7fb93ff7522049dda382413
Enes Doğan <enesdoganlive@hotmail.com>
2024-09-17 11:11:15 +0300

    model error solved

---
+++ HRManagementSystem.UI/Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using HRManagementSystem.Business.Interfaces;
+using HRManagementSystem.UI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace HRManagementSystem.UI.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IGenderService _genderService;
+
+        public AccountController(IGenderService genderService)
+        {
+            _genderService = genderService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> SignUp()
+        {
+            var response = await _genderService.GetAllAsync();
+            var model = new UserCreateModel()
+            {
+                Genders = new SelectList(response.Data,"Id","Definition")
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(UserCreateModel model)
+        {
+            return View(model);
+        }
+    }
+}
--- HRManagementSystem.UI/Models/UserCreateModel.cs
+++ HRManagementSystem.UI/Models/UserCreateModel.cs
@@ -1,6 +1,16 @@
-﻿namespace HRManagementSystem.UI.Models
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace HRManagementSystem.UI.Models
 {
     public class UserCreateModel
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public string PhoneNumber { get; set; }
+        public int GenderId { get; set; }
+        public SelectList Genders { get; set; }
     }
 }
