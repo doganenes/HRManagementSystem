2f9588299cc4f472da81f60edfa1b229a1719ad7
Enes Doğan <enesdoganlive@hotmail.com>
2024-09-24 21:33:36 +0300

    added application controller

--- HRManagementSystem.UI/Controllers/ApplicationController.cs
+++ HRManagementSystem.UI/Controllers/ApplicationController.cs
@@ -1,6 +1,58 @@
-﻿namespace HRManagementSystem.UI.Controllers
+﻿using HRManagementSystem.Business.Interfaces;
+using HRManagementSystem.Dtos;
+using HRManagementSystem.UI.Extensions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HRManagementSystem.UI.Controllers
 {
-    public class ApplicationController
+    [Authorize(Roles = "Admin")]
+    public class ApplicationController : Controller
     {
+        private readonly IAdvertisementService _advertisementService;
+
+        public ApplicationController(IAdvertisementService advertisementService)
+        {
+            _advertisementService = advertisementService;
+        }
+
+        public async Task<IActionResult> List()
+        {
+            var response = await _advertisementService.GetAllAsync();
+            return this.ResponseView(response);
+        }
+
+        public IActionResult Create()
+        {
+            return View(new AdvertisementCreateDto());
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Create(AdvertisementCreateDto dto)
+        {
+            var response = await _advertisementService.CreateAsync(dto);
+            return this.ResponseRedirectAction(response, "List");
+        }
+
+        public async Task<IActionResult> Update(int id)
+        {
+            var response = await _advertisementService.GetByIdAsync<AdvertisementUpdateDto>(id);
+            return this.ResponseView(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(AdvertisementUpdateDto dto)
+        {
+            var response = await _advertisementService.UpdateAsync(dto);
+            return this.ResponseRedirectAction(response, "List");
+        }
+
+        public async Task<IActionResult> Remove(int id)
+        {
+            var response = await _advertisementService.RemoveAsync(id);
+            return this.ResponseRedirectAction(response, "List");
+        }
+
     }
 }
--- HRManagementSystem.UI/Extensions/ControllerExtensions.cs
+++ HRManagementSystem.UI/Extensions/ControllerExtensions.cs
@@ -42,12 +42,10 @@ namespace HRManagementSystem.UI.Extensions
             return controller.View(response.Data);
         }
 
-        public static IActionResult ResponseRedirectToAction(this Controller controller, IResponse response, string actionName)
+        public static IActionResult ResponseRedirectAction(this Controller controller, IResponse response, string actionName)
         {
             if (response.ResponseType == ResponseType.NotFound)
-            {
                 return controller.NotFound();
-            }
             return controller.RedirectToAction(actionName);
         }
 
